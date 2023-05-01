7179e5442432a88d77d1bffd6a235169001a5676
nsy033 <nsy033@kaist.ac.kr>
2023-05-02 03:32:06 +0900

    Implement Calendar with random achievement values

---
+++ src/elements/Calendar/index.js
@@ -0,0 +1,165 @@
+import './style.css';
+import React, { useEffect, useState, useCallback } from 'react';
+import { Icon } from '@iconify/react';
+
+function Calendar() {
+  const URLSplit = window.document.URL.split('/');
+  const timezone = URLSplit[URLSplit.length - 1];
+  const colorsets = {
+    morning: ['#FFCA2D', '#FFE9A9'],
+    day: ['#8CD735', '#D8EDC0'],
+    night: ['#3F51B5', '#CED3F0'],
+    gray: '#EEEEEE',
+  };
+  const emptyFilling = {
+    width: '50px',
+    height: '50px',
+    textAlign: 'center',
+    float: 'left',
+    background: '#FFFFFF',
+    borderRadius: '50%',
+  };
+  const deepFilling = JSON.parse(JSON.stringify(emptyFilling));
+  deepFilling.background = colorsets[timezone][0];
+  const lightFilling = JSON.parse(JSON.stringify(emptyFilling));
+  lightFilling.background = colorsets[timezone][1];
+  const grayFilling = JSON.parse(JSON.stringify(emptyFilling));
+  grayFilling.background = colorsets['gray'];
+  if (timezone == 'night') deepFilling['color'] = '#FFFFFF';
+
+  const today = {
+    year: new Date().getFullYear(), //오늘 연도
+    month: new Date().getMonth() + 1, //오늘 월
+    date: new Date().getDate(), //오늘 날짜
+    day: new Date().getDay(), //오늘 요일
+  };
+  const weekdays = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT']; //일주일
+  const [selectedYear, setSelectedYear] = useState(today.year); //현재 선택된 연도
+  const [selectedMonth, setSelectedMonth] = useState(today.month); //현재 선택된 달
+  const [dateTotalCount, setDateTotalCount] = useState(
+    new Date(selectedYear, selectedMonth, 0).getDate()
+  );
+
+  const MonthNames = [
+    'Jan',
+    'Feb',
+    'Mar',
+    'Apr',
+    'May',
+    'June',
+    'July',
+    'Aug',
+    'Sep',
+    'Oct',
+    'Nov',
+    'Dec',
+  ];
+  let years = [];
+  let months = [];
+  for (let i = today.year - 10; i < today.year + 10; i++) years.push(i);
+  for (let i = 1; i <= 12; i++) months.push(i);
+
+  const goPrevMonth = () => {
+    if (selectedMonth === 1) {
+      setSelectedMonth(12);
+      setSelectedYear(selectedYear - 1);
+    } else {
+      setSelectedMonth(selectedMonth - 1);
+    }
+  };
+  const goNextMonth = () => {
+    if (selectedMonth === 12) {
+      setSelectedMonth(1);
+      setSelectedYear(selectedYear + 1);
+    } else {
+      setSelectedMonth(selectedMonth + 1);
+    }
+  };
+
+  const displayWeekdays = () => {
+    let weekdayLabels = [];
+    weekdays.forEach((weekday, index) => {
+      weekdayLabels.push(
+        <div
+          key={weekday}
+          className={
+            index == 0 ? 'sunday' : index == 6 ? 'saturday' : 'weekday'
+          }
+        >
+          {weekday}
+        </div>
+      );
+    });
+
+    return weekdayLabels;
+  };
+
+  const displayDates = useCallback(() => {
+    let dates = [];
+
+    for (const weekday of weekdays) {
+      const firstDay = new Date(selectedYear, selectedMonth - 1, 1).getDay();
+      if (weekdays[firstDay] === weekday) {
+        for (let i = 1; i <= dateTotalCount; i++) {
+          const day = new Date(selectedYear, selectedMonth - 1, i).getDay();
+          const achievement = Math.floor(Math.random() * 3);
+          dates.push(
+            <div
+              key={i}
+              className={
+                today.year == selectedYear &&
+                today.month == selectedMonth &&
+                today.date == i
+                  ? 'today'
+                  : day == 0
+                  ? 'sunday'
+                  : day == 6
+                  ? 'saturday'
+                  : 'weekday'
+              }
+            >
+              <div
+                className="outline"
+                style={
+                  new Date(selectedYear, selectedMonth - 1, i).getTime() >
+                  new Date().getTime()
+                    ? emptyFilling
+                    : achievement == 0
+                    ? grayFilling
+                    : achievement == 1
+                    ? lightFilling
+                    : deepFilling
+                }
+              >
+                {i}
+              </div>
+            </div>
+          );
+        }
+      } else {
+        dates.push(<div key={weekday} className="weekday"></div>);
+      }
+    }
+
+    return dates;
+  }, [selectedYear, selectedMonth, dateTotalCount]);
+
+  return (
+    <div className="calendarContainer">
+      <div className="pagination">
+        <button onClick={goPrevMonth}>
+          <Icon icon="material-symbols:arrow-back-ios-new-rounded" />
+        </button>
+        <div className="monthName">{MonthNames[selectedMonth - 1]}</div>
+        <button onClick={goNextMonth}>
+          <Icon icon="material-symbols:arrow-forward-ios-rounded" />
+        </button>
+      </div>
+
+      <div className="week">{displayWeekdays()}</div>
+      <div className="date">{displayDates()}</div>
+    </div>
+  );
+}
+
+export default Calendar;
--- src/elements/SubPage/index.js
+++ src/elements/SubPage/index.js
@@ -2,6 +2,8 @@ import './style.css';
 import React, { useEffect, useState } from 'react';
 import * as ROUTES from '../../constants/routes';
 
+import Calendar from '../Calendar';
+
 function SubPage() {
   const URLSplit = window.document.URL.split('/');
   const timezone = URLSplit[URLSplit.length - 1];
@@ -14,6 +16,7 @@ function SubPage() {
       <div>
         <a href={ROUTES.ROUTINE + '/' + timezone + '/wakeup'}> Wake Up </a>
       </div>
+      <Calendar />
     </div>
   );
 }
