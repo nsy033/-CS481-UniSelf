511dffd6c88dac831e1e6cb9de09a2d27853b939
nsy033 <nsy033@kaist.ac.kr>
2023-05-13 04:34:39 +0900

    Implement companion deletion

--- src/elements/RoutineList/index.js
+++ src/elements/RoutineList/index.js
@@ -16,50 +16,56 @@ function RoutineList(props) {
 
   return (
     <div className="RoutineList">
-      <li className="sub-routines-box">
-        {pageType === 'mainPage' ?
-        <a href={ROUTES.SUB + '/morning'} className="morning">
-          Morning 🌻
-        </a>
-        :
-        <div onClick={()=>setters.morning()} className="morning">
-          Morning 🌻
-        </div>
-        }
-        <ul>
-          <li>Wake up at 9AM</li>
-          <li>Check mail box</li>
-        </ul>
-      </li>
-      <li className="sub-routines-box">
-        {pageType === 'mainPage' ?
-        <a href={ROUTES.SUB + '/day'} className="day">
-          Day 🌈
-        </a>
-        :
-        <div onClick={()=>setters.day()} className="day">
-          Day 🌈
-        </div>
-        }
-        <ul>
-          <li>Use less SNS</li>
-        </ul>
-      </li>
-      <li className="sub-routines-box">
-        {pageType === 'mainPage' ?
-        <a href={ROUTES.SUB + '/night'} className="night">
-          Night 🌙
-        </a>
-        :
-        <div onClick={()=>setters.night()} className="night">
-          Night 🌙
-        </div>
-        }
-        <ul>
-          <li>Exercise more than 1 hr.</li>
-          <li>Write diary</li>
-        </ul>
-      </li>
+      {pageType === 'mainPage' ? (
+        <li className="sub-routines-box">
+          <a href={ROUTES.SUB + '/morning'} className="morning">
+            Morning 🌻
+          </a>
+          <ul>
+            <li>Wake up at 9AM</li>
+            <li>Check mail box</li>
+          </ul>
+        </li>
+      ) : (
+        <li className="sub-routines-box">
+          <div onClick={() => setters.morning()} className="morning-simple">
+            Morning 🌻
+          </div>
+        </li>
+      )}
+      {pageType === 'mainPage' ? (
+        <li className="sub-routines-box">
+          <a href={ROUTES.SUB + '/day'} className="day">
+            Day 🌈
+          </a>
+          <ul>
+            <li>Use less SNS</li>
+          </ul>
+        </li>
+      ) : (
+        <li className="sub-routines-box">
+          <div onClick={() => setters.day()} className="day-simple">
+            Day 🌈
+          </div>
+        </li>
+      )}
+      {pageType === 'mainPage' ? (
+        <li className="sub-routines-box">
+          <a href={ROUTES.SUB + '/night'} className="night">
+            Night 🌙
+          </a>
+          <ul>
+            <li>Exercise more than 1 hr.</li>
+            <li>Write diary</li>
+          </ul>
+        </li>
+      ) : (
+        <li className="sub-routines-box">
+          <div onClick={() => setters.night()} className="night-simple">
+            Night 🌙
+          </div>
+        </li>
+      )}
     </div>
   );
 }
