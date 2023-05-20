a5b34c2ee659bc0bde51397ed353d4236b833be4
Yumin Cho <dbals081216@gmail.com>
2023-05-20 23:03:05 +0900

    Change RoutineCreateButton TimeSelector

--- src/elements/CreateRoutine/createModal.js
+++ src/elements/CreateRoutine/createModal.js
@@ -52,8 +52,28 @@ function CreateModal({ setModalOpen }) {
           <div className='customBlock'>
             <label>Goal</label>
             <hr/>
-            <TimePickerButton/>
-            {/* <TimePickerModal/> */} {/* error occurrence point*/}
+            More than
+            <select className='dropdownCustomTime'>
+              <option value="0">0</option>
+              <option value="1">1</option>
+              <option value="2">2</option>
+            </select>
+            Hour
+            <select className='dropdownCustomTime'>
+              <option value="0">0</option>
+              <option value="5">5</option>
+              <option value="10">10</option>
+              <option value="15">15</option>
+              <option value="20">20</option>
+              <option value="25">25</option>
+              <option value="30">30</option>
+              <option value="35">35</option>
+              <option value="40">40</option>
+              <option value="45">45</option>
+              <option value="50">50</option>
+              <option value="55">55</option>
+            </select>
+            Min
           </div>
           <div className='customBlock'>
             <label>Adjstment Period</label>
