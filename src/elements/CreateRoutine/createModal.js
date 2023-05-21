09e1baea6c62c9f9db0c8587b58e47a1cfd6eea1
nsy033 <nsy033@kaist.ac.kr>
2023-05-21 23:24:51 +0900

    Connect create button with combo checker

--- src/elements/CreateRoutine/createModal.js
+++ src/elements/CreateRoutine/createModal.js
@@ -7,12 +7,12 @@ import TimePickerButton from './timePickerButton';
 import TimePickerModal from './timePickerModal';
 import { Timeline } from '@mui/icons-material';
 
-function CreateModal({ setModalOpen }) {
-
+function CreateModal(props) {
+  const { setModalOpen, onAddBtnClick } = props;
   const URLSplit = window.document.URL.split('/');
 
   var timezone =
-  URLSplit.length >= 5 ? URLSplit[URLSplit.length - 1] : 'morning';
+    URLSplit.length >= 5 ? URLSplit[URLSplit.length - 1] : 'morning';
 
   const colorsets = {
     morning: ['#FFCA2D', '#FFE9A9'],
@@ -21,56 +21,61 @@ function CreateModal({ setModalOpen }) {
   };
 
   const emptyFilling = {
-      background: '#FFFFFF'
+    background: '#FFFFFF',
   };
 
   const deepFilling = JSON.parse(JSON.stringify(emptyFilling));
   deepFilling.background = colorsets[timezone][0];
   if (timezone == 'night') {
-      deepFilling.color = '#FFFFFF';
+    deepFilling.color = '#FFFFFF';
   }
 
   const closeModal = () => {
     setModalOpen(false);
   };
 
-  const hourList = [7, 8, 9, 10, 11]
-  const minuteList = [0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55]
-  const [Selected, setSelected] = useState("");
+  const hourList = [7, 8, 9, 10, 11];
+  const minuteList = [0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55];
+  const [Selected, setSelected] = useState('wakeUp');
 
   const handleSelect = (e) => {
+    console.log('selected in handleselect', Selected);
     setSelected(e.target.value);
   };
 
   return (
-      <div className='createModal' align="left"> 
-        <div align="middle" className='modalTitle'>
-          <b>New Morning Routine</b>
+    <div className="createModal" align="left">
+      <div align="middle" className="modalTitle">
+        <b>New Morning Routine</b>
+      </div>
+      <form className="modalBody">
+        <div className="customBlock">
+          <label>Routine</label>
+          <hr />
+          <select
+            className="dropdownCustom"
+            onChange={handleSelect}
+            value={Selected}
+          >
+            <option value="wakeUp">🛏️ Wake up</option>
+            <option value="goOut">🚪 Go out</option>
+            <option value="exercise">🏃 Exercise</option>
+          </select>
         </div>
-        <form className='modalBody'>
-          <div className='customBlock'>
-            <label>Routine</label>
-            <hr/>
-            <select className='dropdownCustom'onChange={handleSelect} value={Selected}>
-              <option value="wakeUp">🛏️ Wake up</option>
-              <option value="goOut">🚪 Go out</option>
-              <option value="exercise">🏃 Exercise</option>
-            </select>
-          </div>
-          {(Selected === "" || Selected === "wakeUp" || Selected === "goOut")
-          && <div className='customBlock'>
+        {(Selected === '' || Selected === 'wakeUp' || Selected === 'goOut') && (
+          <div className="customBlock">
             <label>Goal</label>
-            <hr/>
+            <hr />
             Ealier than
-            <select className='dropdownCustomTime'>
-            {hourList.map((item) => (
+            <select className="dropdownCustomTime">
+              {hourList.map((item) => (
                 <option value={item} key={item}>
                   {item}
                 </option>
               ))}
             </select>
             :
-            <select className='dropdownCustomTime'>
+            <select className="dropdownCustomTime">
               {minuteList.map((item) => (
                 <option value={item} key={item}>
                   {item}
@@ -78,19 +83,20 @@ function CreateModal({ setModalOpen }) {
               ))}
             </select>
             AM
-          </div>}
-          {Selected=="exercise"
-          && <div className='customBlock'>
+          </div>
+        )}
+        {Selected == 'exercise' && (
+          <div className="customBlock">
             <label>Goal</label>
-            <hr/>
+            <hr />
             More than
-            <select className='dropdownCustomTime'>
+            <select className="dropdownCustomTime">
               <option value="0">0</option>
               <option value="1">1</option>
               <option value="2">2</option>
             </select>
             Hour
-            <select className='dropdownCustomTime'>
+            <select className="dropdownCustomTime">
               {minuteList.map((item) => (
                 <option value={item} key={item}>
                   {item}
@@ -98,28 +104,37 @@ function CreateModal({ setModalOpen }) {
               ))}
             </select>
             Min
-          </div>}
-          <div className='customBlock'>
-            <label>Adjstment Period</label>
-            <hr/>
-            <select className='dropdownCustom'>
-              <option value="none">None</option>
-              <option value="oneWeek">1 week</option>
-              <option value="twoWeek">2 weeks</option>
-              <option value="threeWeek">3 weeks</option>
-            </select>
           </div>
-          <br/>
-        </form>
-        <div className='modalFooter'>
-            <button className='xButton' onClick={closeModal}>
-                Cancel
-            </button>
-            <button className='oButton' style={deepFilling}>
-              Create
-            </button>
-          </div>
+        )}
+        <div className="customBlock">
+          <label>Adjstment Period</label>
+          <hr />
+          <select className="dropdownCustom">
+            <option value="none">None</option>
+            <option value="oneWeek">1 week</option>
+            <option value="twoWeek">2 weeks</option>
+            <option value="threeWeek">3 weeks</option>
+          </select>
+        </div>
+        <br />
+      </form>
+      <div className="modalFooter">
+        <button className="xButton" onClick={closeModal}>
+          Cancel
+        </button>
+        <button
+          className="oButton"
+          style={deepFilling}
+          onClick={() => {
+            console.log('onclick', Selected);
+            onAddBtnClick(Selected);
+            closeModal();
+          }}
+        >
+          Create
+        </button>
       </div>
+    </div>
   );
 }
 
