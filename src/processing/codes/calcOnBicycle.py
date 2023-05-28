d4d236f689721220db00a283d5c7d40552ad35ae
Yumin Cho <dbals081216@gmail.com>
2023-05-28 21:29:22 +0900

    Done: Preprocess data on PhysicalActivityEventEntity ON_BICYCLE

--- src/processing/codes/calcOnBicycle.py
+++ src/processing/codes/calcOnBicycle.py
@@ -45,45 +45,96 @@ dates = {
     "USER1": date700s, "USER2": date1500s, "USER3" : date3000s, "USER4" : date700s, "USER5" : date1500s
 }
 
-df = pd.read_csv("src/processing/dataset/P0701/PhysicalActivityEventEntity-5572736000.csv")
+# resultDF = pd.DataFrame()
 
-print("Get the type with the highest confidence for each unique timestamp...")
-df = df.loc[df.groupby(['timestamp'], sort=False)['confidence'].idxmax()][['timestamp', 'confidence', 'type']]
+# for i in range(1, 6):
+#     uID = "USER" + str(i)
+#     users[uID] = list(filter(lambda pNum: pNum not in absentUsers, users[uID]))
+#     print("For making USER", i, ", extracting participants", users[uID])
 
-print("Clear data from 5/8 to 5/14 ...")
-clear_df = df.loc[
-        (1557241200000 <= df["timestamp"]) & (df["timestamp"] < 1557846000000)
-    ]
+#     print("Merge into one dataframe ...")
+#     for user in users[uID]:
+#         for date in dates[uID]:
+#             print("Start for " + user + " on " + str(date))
+#             df = pd.read_csv(
+#                 "src/processing/dataset/%s/PhysicalActivityEventEntity-%d.csv" % (user, date)
+#             )
 
-clear_df.to_csv("src/processing/csvs/calcOnBicycle_test.csv", mode="w")
-clear_df["date"] = pd.to_datetime(clear_df["timestamp"], unit="ms")
+#             if df.empty:
+#                 daily_on_bicycle_time = 0
+#             else:
+#                 df["userID"] = user
+#                 df["datetime"] = pd.to_datetime(df["timestamp"], unit="ms")
+#                 df["datetime"] = pd.DatetimeIndex(df["datetime"]) + timedelta(hours=9)
 
-print("Iter data to find activity time ranges and duration ...")
-start_timestemp = clear_df.loc[0]['timestamp']
-start_date = clear_df.loc[0]['date']
-temp_type = clear_df.loc[0]['type']
-last_timestamp = clear_df.loc[0]['timestamp']
-last_date = clear_df.loc[0]['date']
+#                 # print("Get the type with the highest confidence for each unique timestamp...")
+#                 df = df.loc[df.groupby(['timestamp', 'userID'], sort=False)['confidence'].idxmax()][['timestamp', 'confidence', 'type', 'userID']]
 
-activity_gap_df = pd.DataFrame(columns=['start_timestamp', 'last_timestamp', 'start_date', 'last_date', 'type', 'gap'])
+#                 clear_df = df
 
-for index, row in clear_df[1:].iterrows():
-    if temp_type == row['type']:
-        last_timestamp = row['timestamp']
-        last_date = row['date']
-    else:
-        gap = last_timestamp - start_timestemp
-        temp = {'start_timestamp': str(start_timestemp), 'last_timestamp': str(last_timestamp), 'start_date': start_date, 'last_date': last_date, 'type': temp_type, 'gap': (last_date - start_date).total_seconds()}
-        activity_gap_df = activity_gap_df._append(temp, ignore_index=True)
-        start_timestemp = row['timestamp']
-        start_date = row['date']
-        temp_type = row["type"]
-        last_timestamp = row['timestamp']
-        last_date = row['date']
+#                 # clear_df.to_csv("src/processing/csvs/calcOnBicycle_test.csv", mode="w")
+#                 clear_df["date"] = pd.to_datetime(clear_df["timestamp"], unit="ms")
 
-print("Drop the non-continuous date ...")
-filtered_df = activity_gap_df[(activity_gap_df['type'] == 'ON_BICYCLE') & (activity_gap_df['gap'] != 0)]
+#                 # print("Iter data to find activity time ranges and duration ...")
+#                 start_timestemp = clear_df.loc[0]['timestamp']
+#                 start_date = clear_df.loc[0]['date']
+#                 temp_type = clear_df.loc[0]['type']
+#                 last_timestamp = clear_df.loc[0]['timestamp']
+#                 last_date = clear_df.loc[0]['date']
 
-print("Calculate time for ON_BICYCLE activity...")
-daily_on_bicycle_time = filtered_df['gap'].sum(axis=0)
-print(daily_on_bicycle_time)
+#                 activity_gap_df = pd.DataFrame(columns=['start_timestamp', 'last_timestamp', 'start_date', 'last_date', 'type', 'gap'])
+
+#                 for index, row in clear_df[1:].iterrows():
+#                     if temp_type == row['type']:
+#                         last_timestamp = row['timestamp']
+#                         last_date = row['date']
+#                     else:
+#                         gap = last_timestamp - start_timestemp
+#                         temp = {'start_timestamp': str(start_timestemp), 'last_timestamp': str(last_timestamp), 'start_date': start_date, 'last_date': last_date, 'type': temp_type, 'gap': (last_date - start_date).total_seconds()}
+#                         activity_gap_df = activity_gap_df._append(temp, ignore_index=True)
+#                         start_timestemp = row['timestamp']
+#                         start_date = row['date']
+#                         temp_type = row["type"]
+#                         last_timestamp = row['timestamp']
+#                         last_date = row['date']
+
+#                 # print("Drop the non-continuous date ...")
+#                 filtered_df = activity_gap_df[(activity_gap_df['type'] == 'ON_BICYCLE') & (activity_gap_df['gap'] != 0)]
+
+#                 # print("Calculate time for ON_BICYCLE activity...")
+#                 daily_on_bicycle_time = filtered_df['gap'].sum(axis=0)
+
+#             result_temp = {'userID': uID, 'date': clear_df["date"][0].date(), 'bicycleTime': daily_on_bicycle_time}
+#             resultDF = resultDF._append(result_temp, ignore_index=True)
+
+# print(resultDF)
+
+finalDF = pd.DataFrame()
+resultDF = pd.read_csv("src/processing/csvs/calcOnBicycle.csv")
+
+resultDF["date__origin"] = resultDF["date"]
+
+resultDF.loc[resultDF['userID'] == 'USER5', 'userID'] = 'USER4'
+
+merged_csv = pd.DataFrame()
+dateMax = -1
+for i in range(1, 6):
+    uID = "USER" + str(i)
+    oneUsers = resultDF.loc[resultDF["userID"] == uID]
+    if dateMax == -1:
+        dateMax = oneUsers["date__origin"].max()
+    print(pd.to_datetime(dateMax), oneUsers.shape[0] - 1)
+    oneUsers["date"]= pd.date_range(
+        pd.to_datetime(dateMax) - timedelta(days=oneUsers.shape[0] - 1),
+        pd.to_datetime(dateMax),
+        freq="D",
+    )
+    oneUsers = oneUsers.loc[oneUsers["date"] >= pd.to_datetime('2019-01-26')]
+    merged_csv = pd.concat([merged_csv, oneUsers], axis=0)
+
+merged_csv = merged_csv.groupby(['userID', 'date']).sum().reset_index().loc[:, ['userID', 'date', 'bicycleTime']]
+merged_csv['bicycleTime'] = merged_csv['bicycleTime'].round().astype(int)
+print("Save the result file ...")
+merged_csv.to_csv("src/processing/csvs/calcOnBicycle_final.csv", mode="w")
+
+print("Done")
